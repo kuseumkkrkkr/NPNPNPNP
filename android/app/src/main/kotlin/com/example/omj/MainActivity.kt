2f1f412c06cd52a08507cfa3c1bbb459d34354a2
cw_lee_kus <cw20208021@gmail.com>
2025-08-24 15:18:28 +0900

    first AID

---
+++ android/app/src/main/kotlin/com/example/omj/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.omj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Home_Page extends StatelessWidget {
+  const Home_Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Home Page'));
+  }
+}
